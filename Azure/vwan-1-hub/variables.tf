5b8ac169ac54f27961e61ebb5a76088b936bc4ab
Felix Bueltmann <felix@fxc.one>
2021-07-28 12:53:26 +0200

    added address prefix as var

--- Azure/vwan-1-hub/variables.tf
+++ Azure/vwan-1-hub/variables.tf
@@ -5,3 +5,6 @@ variable "prefix" {
       description = "An abbreviation which represents your resource group as well as it is added in front of some resources"
       default = "fbu-cgf-devops"
 }
+variable "address_prefix" {
+  default = "10.0.0.0/23"
+}
