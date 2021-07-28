e4ca99cabeb8b9ae8a9aa4d09090051fc0d95df5
Felix Bueltmann <felix@fxc.one>
2021-07-28 14:10:43 +0200

    minor changes

--- Azure/vwan-1-hub/README.md
+++ Azure/vwan-1-hub/README.md
@@ -1,1 +1,5 @@
 # Virtual WAN including 1 hub
+
+This script will setup a resource group, a virtual WAN and in there a HUB.
+The Location, Names and the HUB IP-Range will be defined in the variables.tf file.
+
--- Azure/vwan-1-hub/variables.tf
+++ Azure/vwan-1-hub/variables.tf
@@ -3,7 +3,6 @@ variable "location" {
 }
 variable "prefix" {
       description = "An abbreviation which represents your resource group as well as it is added in front of some resources"
-      default = "fbu-cgf-devops"
 }
 variable "address_prefix" {
   default = "10.1.0.0/24"
